1bfad60a5f4aefa324ce316975b8b86c183ffc7f
faiz <fk9653047@gmail.com>
2025-09-20 18:05:34 +0530

    fix path error

--- src/main.jsx
+++ src/main.jsx
@@ -10,49 +10,34 @@ import CoursesPage from './Pages/Courses'
 import Dashboard from './Pages/admin/Dashboard'
 import AdminLayouts from './layouts/AdminLayouts'
 import AboutForm from './Pages/admin/AboutFrom'
-import { Settings } from 'lucide-react'
 import MainSettings from './Pages/admin/MainSettings'
 import HelpCare from './Pages/HalpCenter'
 import PrivacyPolicy from './Pages/PrivacyPolicy'
 
-
 const router = createBrowserRouter(
   createRoutesFromElements(
-<>
+    <>
+      <Route path="/" element={<Layout />}>
+        <Route index={true} element={<HomePage />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<ContactForm />} />
+        <Route path="courses" element={<CoursesPage />} />
+        <Route path="helpcare" element={<HelpCare />} />
+        <Route path="privacypolicy" element={<PrivacyPolicy />} />
+      </Route>
 
-        <Route path="/" element={<Layout />}>
-
-          <Route index={true} element={<HomePage/>} />
-          
-          <Route path='/about' element={<About />} />
-
-          <Route path='/contact' element={<ContactForm/>} />
-
-          <Route path='/courses' element={<CoursesPage/>} />
-
-          <Route path='/helpcare' element={<HelpCare/>}/>
-
-          <Route path='/privacypolicy' element={<PrivacyPolicy/>}/>
-
-
-        </Route>
-
-
-        /////////// admin Route
-        <Route path='/admin' element={<AdminLayouts/>}>
-
-          <Route index={true} element={<Dashboard/>} />
-
-          <Route path='aboutfrom' element={<AboutForm/>}/>
-
-          <Route path='settings' element={<MainSettings/>}/>
-
-        </Route>
-</>
-  ))
+      {/* Admin Routes */}
+      <Route path="/admin" element={<AdminLayouts />}>
+        <Route index={true} element={<Dashboard />} />
+        <Route path="aboutfrom" element={<AboutForm />} />
+        <Route path="settings" element={<MainSettings />} />
+      </Route>
+    </>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 )
